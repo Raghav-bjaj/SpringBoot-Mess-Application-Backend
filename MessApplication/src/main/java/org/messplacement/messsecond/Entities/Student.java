057449b05a369dd016b881a35c41d2a79e61d871
Abhishek Choudhary <132264630+Raghav-bjaj@users.noreply.github.com>
2025-06-03 16:34:39 +0530

    Update Student.java

--- MessApplication/src/main/java/org/messplacement/messsecond/Entities/Student.java
+++ MessApplication/src/main/java/org/messplacement/messsecond/Entities/Student.java
@@ -1,80 +1,85 @@
+```java
 package org.messplacement.messsecond.Entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.time.LocalDate;
 
-
 @Entity
+@Table(name = "students")
 public class Student {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long AUID;
+    private String reg;
+    private LocalDate date;
+    private boolean breakfast;
+    private boolean lunch;
+    private boolean dinner;
+    private int total;
 
-
-    String Reg;
-    @ Column(nullable = true)
-    LocalDate date;
-
-    boolean breakfast;
-    boolean lunch;
-    boolean dinner;
-
-    @ Column(nullable = true)
-    int total;
-
-    public Student(String reg, LocalDate date, boolean Breakfast, boolean Lunch, boolean Dinner, int total) {
-        this.Reg = reg;
-        this.date = date;
-        this.breakfast = Breakfast;
-        this.lunch = Lunch;
-        this.dinner = Dinner;
-        this.total = total;
-
-    }
-
+    // Default constructor
     public Student() {
-        super();
     }
 
-    public long getId() {
-        return AUID;
+    // Constructor
+    public Student(String reg, LocalDate date, boolean breakfast, boolean lunch, boolean dinner, int total) {
+        this.reg = reg;
+        this.date = date;
+        this.breakfast = breakfast;
+        this.lunch = lunch;
+        this.dinner = dinner;
+        this.total = total;
     }
-    public void setId(long AUID) { this.AUID = AUID; }
-    public void setreg(String  reg) {
-        this.Reg = reg;
+
+    // Getters and Setters
+    public String getReg() {
+        return reg;
     }
-    public String getreg() {
-        return Reg;
+
+    public void setReg(String reg) {
+        this.reg = reg;
     }
+
     public LocalDate getDate() {
         return date;
     }
+
     public void setDate(LocalDate date) {
         this.date = date;
     }
+
     public boolean getBreakfast() {
         return breakfast;
     }
-    public void setBreakfast(Boolean breakfast) {
+
+    public void setBreakfast(boolean breakfast) {
         this.breakfast = breakfast;
     }
+
     public boolean getLunch() {
         return lunch;
     }
-    public void setLunch(Boolean lunch) {
+
+    public void setLunch(boolean lunch) {
         this.lunch = lunch;
     }
+
     public boolean getDinner() {
         return dinner;
     }
-    public void setDinner(Boolean dinner) {
+
+    public void setDinner(boolean dinner) {
         this.dinner = dinner;
     }
+
     public int getTotal() {
         return total;
     }
+
     public void setTotal(int total) {
         this.total = total;
     }
 }
+```
